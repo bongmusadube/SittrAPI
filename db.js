0cff877dce8c7a211a79cc1d96fc3c9b7c51feab
Bong'musa Dube <bongmusadube1@gmail.com>
2024-08-17 22:36:27 +0200

    updated conneciton string to railway postgres

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
-const pool = new Pool({ 
-    user: 'postgres',
-    host: 'localhost',
-    database: 'sittrdatabase',
-    password: 'ADMIN',
-    port: 5432 
-
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
 });
 
 module.exports = pool;
-
-
